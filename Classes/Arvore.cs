26d427b0067690c204c122be158dc9a8285af06b
AnaCarlaG <47726766+AnaCarlaG@users.noreply.github.com>
2021-02-23 20:46:15 -0300

    Estrutura básica com as classes nó e árvore

---
+++ Classes/Arvore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArvoreGenerica.Classes
+{
+    public class Arvore
+    {
+        private No Raiz;
+
+
+        public Arvore(int dadosRaiz)
+        {
+            this.Raiz = new No(dadosRaiz);
+        }
+
+        public No getRaiz()
+        {
+            return raiz;
+        }
+
+        public No Buscar (int procurado)
+        {
+            Raiz.Buscar(procurado);
+        }
+    }
+}
---
+++ Classes/No.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArvoreGenerica.Classes
+{
+    public class No
+    {
+        private int dados;
+        private List<No> filhos;
+
+        public No(int dados)
+        {
+            this.dados = dados;
+            this.filhos = new List<No>();
+        }
+
+        public No AddFilho(int dadosFilho)
+        {
+            No n = new No(dadosFilho);
+            filhos.Add(n);
+            return n;
+        }
+
+        public int getDados()
+        {
+            return dados;
+        }
+
+        public List<No> getFilhos()
+        {
+            return filhos;
+        }
+
+        public No Buscar (int procurado)
+        {
+            if(filhos.Exists(m => m.dados == procurado))
+            {
+                return filhos.Find(m => m.dados == procurado);
+            }
+            return null;
+        }
+    }
+}
